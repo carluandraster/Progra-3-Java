43c8e1e1ae35bd5c442ff1c499ddc01b05243ad3
carluandraster <99754800+carluandraster@users.noreply.github.com>
2025-04-20 14:51:13 -0300

    Add files via upload

---
+++ TP 5/Ejercicio4/Arquero.java
@@ -0,0 +1,35 @@
+
+public class Arquero extends Personaje {
+    private int cantidadFlechas;
+    private static int vitalidadInicial = 500;
+
+    public Arquero(String nombre) {
+        super(nombre);
+        this.vitalidad = vitalidadInicial;
+        this.alcance = 100;
+        this.danio = 15;
+        this.cantidadFlechas = 20;
+    }
+
+    public int getCantidadFlechas() {
+        return cantidadFlechas;
+    }
+
+    @Override
+    public boolean ataca(Personaje personaje) {
+        if (this.cantidadFlechas > 0)
+            this.cantidadFlechas--;
+        boolean aux = super.ataca(personaje);
+        if (this.cantidadFlechas == 0) {
+            this.alcance = 5;
+            this.danio = 5;
+        }
+        return aux;
+    }
+
+    @Override
+    public void respawn() {
+        this.vitalidad = vitalidadInicial;
+        super.respawn();
+    }
+}
