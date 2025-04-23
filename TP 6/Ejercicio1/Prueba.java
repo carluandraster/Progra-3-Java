1f36a0ace2b2b475f88b57b232ebabc5de45d23c
carluandraster <99754800+carluandraster@users.noreply.github.com>
2025-04-22 22:28:27 -0300

    Add files via upload

--- TP 6/Ejercicio1/Prueba.java
+++ TP 6/Ejercicio1/Prueba.java
@@ -1,5 +1,13 @@
 public class Prueba {
     public static void main(String[] args) {
-        Usuario u1 = new Usuario("Andres", "contrasenia");
+        try {
+            Usuario u1 = new Usuario("Andres", "contrasenia");
+            Usuario u2 = new Usuario("Carlos", "abcdefg");
+            Usuario u3 = new Usuario("Fernando", "nullefg");
+            Usuario u4 = new Usuario("Maria", "1contra");
+        } catch (Exception e) {
+            System.err.println(e);
+        }
+
     }
 }
