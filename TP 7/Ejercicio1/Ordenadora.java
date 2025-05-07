a783903c7a867dd13b68fb1026a81ef4a52fbaad
carluandraster <99754800+carluandraster@users.noreply.github.com>
2025-05-07 16:21:34 -0300

    Add files via upload

---
+++ TP 7/Ejercicio1/Ordenadora.java
@@ -0,0 +1,27 @@
+public class Ordenadora {
+    // Devuelve el índice donde está el mínimo
+    private static int inMin(int indiceInicial,Comparable[] array){
+        Comparable min = array[indiceInicial];
+        int iMin = indiceInicial;
+        for (int i = 1; i < array.length; i++) {
+            if (array[i].compareTo(min) < 0) {
+                min = array[i];
+                iMin = i;
+            }
+        }
+        return iMin;
+    }
+
+    public static void ordenar(Comparable[] array){
+        int longitud = array.length;
+        Comparable aux;
+        int iMin;
+        for (int i = 0; i < array.length; i++) {
+            iMin = inMin(i, array);
+            // Intercambiar array[i] con array[iMin]
+            aux = array[i];
+            array[i] = array[iMin];
+            array[iMin] = aux;
+        }
+    }
+}
