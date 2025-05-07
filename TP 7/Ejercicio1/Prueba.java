3f00f1f00cc2bdcf072e42dcfe34fcc699aaa4f9
carluandraster <99754800+carluandraster@users.noreply.github.com>
2025-05-07 16:28:24 -0300

    Add files via upload

---
+++ TP 7/Ejercicio1/Prueba.java
@@ -0,0 +1,9 @@
+public class Prueba {
+    public static void main(String[] args) {
+        Persona persona1 = new Persona("Perez", "Carlos", "Colon 3212");
+        Persona persona2 = new Persona("Perez", "Carlos", "Colon 3212");
+        Persona persona3 = new Persona("Garcia", "Ana", "Mitre 2812");
+        Persona persona4 = new Persona("Alvarez", "Valeria", "San Luis 2812");
+        Persona persona5 = new Persona("Garcia", "Luis", "Matheu 3538");
+    }
+}
