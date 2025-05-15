615e9428c8f7008e6383b4db049c89d400bbb264
carluandraster <99754800+carluandraster@users.noreply.github.com>
2025-05-15 15:10:04 -0300

    Add files via upload

--- TP 7/Ejercicio2/Modelo/OperadoraEmergencias.java
+++ TP 7/Ejercicio2/Modelo/OperadoraEmergencias.java
@@ -1,18 +1,20 @@
 package Ejercicio2.Modelo;
 
 import java.util.ArrayList;
+import java.util.GregorianCalendar;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.Map;
 
-public class OperadoraEmergencias extends ListaConPrioridades<Llamado>{
+public class OperadoraEmergencias extends ListaConPrioridades<Llamado> {
     private ArrayList<Llamado> llamadosAtendidos;
 
-    public OperadoraEmergencias(){
+    public OperadoraEmergencias() {
         super();
         this.llamadosAtendidos = new ArrayList<>();
     }
 
-    public int cantidadLlamados(Socio socio){
+    public int cantidadLlamados(Socio socio) {
         int contador = 0;
         Llamado aux;
 
@@ -37,22 +39,51 @@ public class OperadoraEmergencias extends ListaConPrioridades<Llamado>{
         return contador;
     }
 
-    public Iterator<Llamado> getLlamadosAtendidos(){
+    public Iterator<Llamado> getLlamadosAtendidos() {
         return this.llamadosAtendidos.iterator();
     }
 
-    public Socio getSocioMasEmergencias(){
+    public Socio getSocioMasEmergencias() {
         Iterator<Llamado> it = this.llamadosAtendidos.iterator();
         HashMap<Socio, Integer> sociosConEmergencias = new HashMap<>();
-        Socio aux;
+        Socio aux = null;
+        int maximo = -1;
 
+        // Recopilo a los socios con sus ocurrencias
         while (it.hasNext()) {
             aux = it.next().getSocio();
             if (sociosConEmergencias.containsKey(aux)) {
-                sociosConEmergencias.replace(aux,sociosConEmergencias.get(aux)+1);
+                sociosConEmergencias.replace(aux, sociosConEmergencias.get(aux) + 1);
             } else {
-                
+                sociosConEmergencias.put(aux, 0);
             }
         }
+
+        // Recorro HashMap
+        for (Map.Entry<Socio, Integer> entry : sociosConEmergencias.entrySet()) {
+            // clave= entry.getKey()
+            // valor= entry.getValue()
+            if (entry.getValue() > maximo) {
+                aux = entry.getKey();
+                maximo = entry.getValue();
+            }
+        }
+
+        return aux;
     }
+
+    @Override
+    public Llamado getElemento() {
+        Llamado aux = super.getElemento();
+        aux.setFechahoraAtendido(new GregorianCalendar());
+        this.llamadosAtendidos.add(aux);
+        return aux;
+        /*
+         * No podíamos setear fechahoraAtendido en la clase ListaConPrioridades porque
+         * ListaConPrioridades es una cola de tipo genérico. No se sabe de qué tipo son
+         * los objetos que contiene por lo que solo podíamos invocar a los métodos de
+         * Comparable y de Object.
+         */
+    }
+
 }
